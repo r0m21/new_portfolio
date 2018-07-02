00ecf1e5df9ad0ea062da339f0a850ec1f0e7866
r0m21 <romainwenger21@gmail.com>
2018-07-02 17:00:08 +0200

    Modification de la home page + quelques style ajouté.

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,18 @@
+$(function(){
 
-UIkit.nav(element, options);
-UIkit.nav(element).toggle(index, animate);
+    $(".tagline").letterfx({
+
+        "fx":"fly-top spin",
+        "backwards":false,
+        "timing":50,
+        "fx_duration":"3000ms",
+        "letter_end":"stay",
+        "element_end":"stay"
+    });
+
+});
+
+
 
 
 
