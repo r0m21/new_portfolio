20de6de973b8ee2f53de1425349adc11312e7c89
r0m21 <romainwenger21@gmail.com>
2018-07-24 11:31:00 +0200

    Récup des fichiers précédents

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,51 @@
 $(function(){
 
+/* ==================================================================
+                    NO LIBRAIRIES / CODE JQUERY
+====================================================================*/
+
+/* $('body').scrollspy({
+    target: '#navigation',
+    offset: $(window).height() / 2
+});
+ */
+
+// On Scroll
+/* $(window).on('scroll', function() {
+    var wScroll = $(this).scrollTop();
+
+    // Fixed nav
+    wScroll > 1 ? $('#navigation').addClass('fixed-top justify-content-around sticky-nb-bg') : $('#navigation').removeClass('fixed-top justify-content-around sticky-nb-bg');
+    wScroll > 1 ? $('.nav-link').addClass('sticky-nb-liens') : $('.nav-link').removeClass('sticky-nb-liens');
+    wScroll > 1 ? $('#logo').attr('src', 'img/logo.png') : $('#logo').attr('src', 'img/logo-alt.png');
+                 
+}); */
+
+
+/* ==================================================================
+                            LIBRAIRIES
+====================================================================*/
+
+/* AOS.JS */
+
     AOS.init();
- 
-    $('#button-nav,.uk-offcanvas-close').click(function(){
-        $('#button-nav').fadeToggle(1000);
-    }); 
 
-    /* if($('.uk-offcanvas').css("display") == "none"){
-        $('#button-nav').show();
-        alert('oui');
-    } */
-    /* if($('.uk-offcanvas').css("display") == "none" && $('#button-nav').css("display") == "none"){
+/* END AOS.JS */
 
-        $('#button-nav').removeClass('none').addClass('d-block');
-       
-        alert('oui');
-    } */
+/* FULLPAGE.JS */
+
+
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: true
+    });
+    
+
+/* END FULLPAGE.JS */
 
 });
+
 
 
 
